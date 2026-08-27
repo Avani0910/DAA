c1138b857bad5d0aa88917b0fa27e637fb583d5a
Avani0910 <avani.kavathiya131334@marwadiuniversity.ac.in>
2026-08-27 11:56:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (54.49%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        res = []
+        count = 0
+        k = len(nums)
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[count] = nums[i]
+                #res.append(i)
+                count+=1
+        return count
+       
+        
+
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        
